e440a03c87ae400da48282781b6476287ff3b04b
KUHAKU-17 <pablolira17@hotmail.com>
2024-07-17 23:30:14 -0300

    Add project files.

---
+++ src/Journey.Application/UseCases/Trips/Register/RegisterTripValidatior.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Journey.Communication.Requests;
+using Journey.Exception;
+
+namespace Journey.Application.UseCases.Trips.Register
+{
+    public class RegisterTripValidatior : AbstractValidator<RequestRegisterTripJson>
+    {
+        public RegisterTripValidatior()
+        {
+            RuleFor(request => request.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+            
+            RuleFor(request => request.StartDate).GreaterThanOrEqualTo(DateTime.UtcNow.Date).WithMessage(ResourceErrorMessages.DATE_TRIP_MUST_BE_LATER_THAN_TODAY);
+
+            RuleFor(request => request).Must(request => request.EndDate.Date >= request.StartDate.Date).WithMessage(ResourceErrorMessages.END_DATE_TRIP_MUST_BE_LATER_START_DATE);
+        
+        }
+    }
+}
